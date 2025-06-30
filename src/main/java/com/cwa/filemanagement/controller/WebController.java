f6cc177fff26a5dc158b0a6f8934d1eaa15fef72
armandnjapou <armand.njapoungassu@consulting-for.accor.com>
2025-06-30 19:08:08 +0200

    Update templates and WebController

--- src/main/java/com/cwa/filemanagement/controller/WebController.java
+++ src/main/java/com/cwa/filemanagement/controller/WebController.java
@@ -1,15 +1,26 @@
 package com.cwa.filemanagement.controller;
 
 import com.cwa.filemanagement.service.FileService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.IOException;
+import java.util.Objects;
+
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class WebController {
@@ -28,15 +39,39 @@ public class WebController {
     }
 
     @PostMapping("/upload")
-    public String handleFileUpload(@RequestParam("file") MultipartFile file,
-                                  @RequestParam(value = "description", required = false) String description,
-                                  RedirectAttributes redirectAttributes) {
+    public String uploadFile(
+            @RequestParam("file") MultipartFile file,
+            @RequestParam("description") String description,
+            RedirectAttributes redirectAttributes
+    ) {
         try {
             fileService.storeFile(file, description);
-            redirectAttributes.addFlashAttribute("successMessage", "File uploaded successfully: " + file.getOriginalFilename());
-        } catch (Exception e) {
-            redirectAttributes.addFlashAttribute("errorMessage", "Error uploading file: " + e.getMessage());
+            redirectAttributes.addFlashAttribute("successMessage", "File uploaded successfully!");
+        } catch (Exception ex) {
+            redirectAttributes.addFlashAttribute("errorMessage", "File upload failed!");
+            log.error("File upload failed!", ex);
         }
         return "redirect:/upload";
+    }
+
+    @GetMapping("/download/{fileName:.+}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) {
+        var resource = fileService.loadFileAsResource(fileName);
+
+        String contentType = null;
+        try {
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        } catch (IOException e) {
+            log.error("Could not determine file type!", e);
+        }
+
+        if (Objects.isNull(contentType)) {
+            contentType = "application/octet-stream";
+        }
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header("Content-Disposition", "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
     }
 }
