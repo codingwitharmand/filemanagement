a6cd6796578054480a0b317cdc9f055506916340
armandnjapou <armand.njapoungassu@consulting-for.accor.com>
2025-06-30 18:14:03 +0200

    Remove Rest controller and FileUploadResponse.java

--- src/main/java/com/cwa/filemanagement/dto/FileUploadResponse.java
+++
@@ -1,9 +0,0 @@
-package com.cwa.filemanagement.dto;
-
-public record FileUploadResponse(
-        String fileName,
-        String fileDownloadUri,
-        String fileType,
-        long size
-) {
-}
